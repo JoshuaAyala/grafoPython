ef618d440453adeeee1990d3d35289a6187a8b97
Joshua <Joshua4yala@pm.me>
2021-09-27 22:12:29 -0700

    Update grafo.py

--- grafo.py
+++ grafo.py
@@ -3,14 +3,14 @@
 
 # Filename  : grafo.py
 # Autor     : Joshua Ayala
-# Website   : https://github.com/JoshuaAyala/04IDMA/grafo.py
+# Website   : https://github.com/JoshuaAyala/grafoPython/blob/main/grafo.py
 # pep8: 100%
 
 import os, time
-import networkx
-import matplotlib.pyplot as plt
+import networkx						# librería para el manejo de grafos
+import matplotlib.pyplot as plt 	# librería para ver visualmente el grafo
 
-def goBack(grafoExiste, grafo):					# Pregunta si quieres volver a la función menú pasando parametro de existencia de árbol y datos del árbol
+def goBack(grafoExiste, grafo):					# Pregunta si quieres volver a la función menú pasando parametro de existencia del grafo y datos del grafo
 	op = input("¿Desea volver al menú? (S/n)\n  >: ").upper()
 	if(op == "S"):
 		menu(grafoExiste, grafo)
@@ -20,15 +20,11 @@ def goBack(grafoExiste, grafo):					# Pregunta si quieres volver a la función m
 		time.sleep(2)
 		exit()
 
-def getInfo(listElementos):
-	return listElementos
-
-
 def menu(grafoExiste, grafo):
 	op = int(input("\n¿Qué desea hacer?\n\n  [1] Crear grafo.\n  [2] Agregar elemento a mi grafo.\n  [3] Ver mi grafo.\n  [4] Buscar un elemento de mi grafo.\n  [5] Eliminar nodo.\n\n  [0] Salir.\n\n>: "))
 	if(op == 1):
-		Grafo = networkx.Graph() 					# Se crea el árbol sin valores
-		grafoExiste = True									# Variable booleana para comprobación de existencia de árbol
+		Grafo = networkx.Graph() 					# Se crea el grafo 
+		grafoExiste = True									# Variable booleana para comprobación de existencia del grafo
 		print("	+- Grafo creado exitosamente.")	
 		time.sleep(2)
 		menu(grafoExiste, grafo)
@@ -37,20 +33,19 @@ def menu(grafoExiste, grafo):
 			listElementos = []
 			c = int(input("\n¿Cuántas aristas deseas agregar al grafo?\n >: "))
 			for i in range(0, c):
-				qw = input("\nArista >: ")
-				listElementos.append(qw)
-				grafo.add_nodes = listElementos[i]
+				qw = input("\nArista >: ")		# Se insertan los datos
+				listElementos.append(qw)		# Se guardan en un array
+				grafo.add_nodes = listElementos[i] 	# Se meten en el array del grafo
 			print("\n +- Aristas agregadas correctamente.")
 			print("\nCreación de vértices.\n")
 			for w in range(0,c):
-				print("Introduce con qué arista se conectará la arista ", listElementos[w],".")
+				print("Introduce con qué arista se conectará la arista ", listElementos[w],".")	
 				x = input(" >: ")
-				grafo.add_edge(listElementos[w], x)
+				grafo.add_edge(listElementos[w], x)  # Se crea la conexión entre aristas
 		else:
 			print("\n[!] No se ha creado ningún grafo o no tiene elementos.\n")
 			goBack(grafoExiste, grafo)
 		time.sleep(2)
-		getInfo(listElementos)
 		menu(grafoExiste, grafo)
 	elif(op == 3):
 		if(grafoExiste == True):
@@ -58,12 +53,12 @@ def menu(grafoExiste, grafo):
 				print("\n[!] El grafo no tiene elementos que mostrar.\n")
 				goBack(grafoExiste, grafo)
 			else:
-				print(" +- Cantidad de aristas: ", len(grafo.nodes))
+				print(" +- Cantidad de aristas: ", len(grafo.nodes)) 	
 				print(" +- Cantidad de vértices: ", len(grafo.edges))
 				print(" +- Aristas: ", grafo.nodes)
 				print(" +- Vértices: ", grafo.edges)
-				networkx.draw(grafo)
-				plt.show()
+				networkx.draw(grafo)			# Se inicializa la interfaz del grafo
+				plt.show()						# Se visualiza
 				goBack(grafoExiste, grafo)
 		else:
 			print("\n[!] No se ha creado ningún grafo o no tiene elementos.\n")
@@ -76,7 +71,7 @@ def menu(grafoExiste, grafo):
 			else:
 				datoabuscar = input("¿Qué dato deseas buscar?\n>: ")
 				if datoabuscar in grafo:
-					print(grafo.edges(datoabuscar))
+					print(grafo.edges(datoabuscar))		# Se muestran las aristas conectadas a el arista buscado
 				else:
 					print("\n[!] El dato no se encuentra o no existe.\n")
 				goBack(grafoExiste, grafo)
@@ -87,11 +82,11 @@ def menu(grafoExiste, grafo):
 		if(grafoExiste):
 			if(len(grafo.nodes)<= 0):
 				print("\n[!] El grafo no tiene elementos que mostrar.\n")
-				goBack(grafoExiste, grafo)
+				goBack(grafoExiste, grafo)			
 			else:
 				datoaeliminar = input("¿Qué arista deseas eliminar?\n >: ")
 				if datoaeliminar in grafo:
-					grafo.remove_node(datoaeliminar)
+					grafo.remove_node(datoaeliminar)			# Se elimina el arista
 					print(" +- La arista se ha eliminado correctamente.")
 					menu(grafoExiste, grafo)
 				else:
@@ -110,20 +105,12 @@ def menu(grafoExiste, grafo):
 def main():
 	os.system('cls')
 	grafoExiste = False
+	print("      ======================")
+	print("	╔═╗╦═╗╔═╗╔═╗╔═╗╔═╗	")
+	print("	║ ╦╠╦╝╠═╣╠╣ ║ ║╚═╗")
+	print("	╚═╝╩╚═╩ ╩╚  ╚═╝╚═╝")
+	print("      ======================")
 	grafo = networkx.Graph()
 	menu(grafoExiste, grafo)
 	
 main()
-
-# Grafo = networkx.Graph()
-# Grafo.add_node("A")      # add_node("n") agrega "n" como nodo del grafo
-# Grafo.add_node("B")
-# Grafo.add_node("C")
-# Grafo.add_edge("A","B")  # add_edge("e") agrega "e" como borde de un nodo
-# Grafo.add_edge("B","C")
-# Grafo.add_edge("C","U")
-
-# print(len(Grafo.nodes))
-# print(len(Grafo.edges))
-# print(Grafo.nodes)
-# print(Grafo.edges)
